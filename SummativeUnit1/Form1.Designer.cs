84b32c4e592276291d3fde76190766a973527aec
LaurenSteel <laurstee147@ed.amdsb.ca>
2017-09-20 13:31:12 -0400

    Add project files.

---
+++ SummativeUnit1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SummativeUnit1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.capriLabel = new System.Windows.Forms.Label();
+            this.date1Label = new System.Windows.Forms.Label();
+            this.date2Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // capriLabel
+            // 
+            this.capriLabel.AutoSize = true;
+            this.capriLabel.BackColor = System.Drawing.Color.Transparent;
+            this.capriLabel.Font = new System.Drawing.Font("Palatino Linotype", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.capriLabel.ForeColor = System.Drawing.Color.White;
+            this.capriLabel.Location = new System.Drawing.Point(197, 367);
+            this.capriLabel.Name = "capriLabel";
+            this.capriLabel.Size = new System.Drawing.Size(256, 50);
+            this.capriLabel.TabIndex = 0;
+            this.capriLabel.Text = "CAPRICORN";
+            this.capriLabel.Click += new System.EventHandler(this.titlelable_Click);
+            // 
+            // date1Label
+            // 
+            this.date1Label.AutoSize = true;
+            this.date1Label.BackColor = System.Drawing.Color.Transparent;
+            this.date1Label.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.date1Label.ForeColor = System.Drawing.Color.White;
+            this.date1Label.Location = new System.Drawing.Point(12, 204);
+            this.date1Label.Name = "date1Label";
+            this.date1Label.Size = new System.Drawing.Size(134, 26);
+            this.date1Label.TabIndex = 1;
+            this.date1Label.Text = "December 22 ";
+            // 
+            // date2Label
+            // 
+            this.date2Label.AutoSize = true;
+            this.date2Label.BackColor = System.Drawing.Color.Transparent;
+            this.date2Label.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.date2Label.ForeColor = System.Drawing.Color.White;
+            this.date2Label.Location = new System.Drawing.Point(515, 204);
+            this.date2Label.Name = "date2Label";
+            this.date2Label.Size = new System.Drawing.Size(107, 26);
+            this.date2Label.TabIndex = 2;
+            this.date2Label.Text = "January 19";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(634, 426);
+            this.Controls.Add(this.date2Label);
+            this.Controls.Add(this.date1Label);
+            this.Controls.Add(this.capriLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Click1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label capriLabel;
+        private System.Windows.Forms.Label date1Label;
+        private System.Windows.Forms.Label date2Label;
+    }
+}
+
---
+++ SummativeUnit1/Form1.cs
@@ -0,0 +1,44 @@
+﻿// Lauren Steel
+// September 19 2017
+// Constellation Greeting Card
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SummativeUnit1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Click1(object sender, EventArgs e)
+        {
+            //Front Page 
+            BackgroundImage = null;
+            capriLabel.Dispose();
+            date1Label.Dispose();
+            date2Label.Dispose();
+            Refresh();
+        }
+
+        private void titlelable_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
