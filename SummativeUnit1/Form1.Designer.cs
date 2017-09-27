dbbffc68aa3b4fc79d699fc87e06409a6af4ce53
LaurenSteel <laurstee147@ed.amdsb.ca>
2017-09-27 13:19:03 -0400

    finished code

--- SummativeUnit1/Form1.Designer.cs
+++ SummativeUnit1/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.capriLabel = new System.Windows.Forms.Label();
             this.date1Label = new System.Windows.Forms.Label();
             this.date2Label = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // capriLabel
@@ -71,6 +72,18 @@
             this.date2Label.TabIndex = 2;
             this.date2Label.Text = "January 19";
             // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.nameLabel.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.ForeColor = System.Drawing.Color.White;
+            this.nameLabel.Location = new System.Drawing.Point(12, 33);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(155, 26);
+            this.nameLabel.TabIndex = 3;
+            this.nameLabel.Text = "By Lauren Steel";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -78,11 +91,13 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(634, 426);
+            this.Controls.Add(this.nameLabel);
             this.Controls.Add(this.date2Label);
             this.Controls.Add(this.date1Label);
             this.Controls.Add(this.capriLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Capricorn Card";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Click += new System.EventHandler(this.Click1);
             this.ResumeLayout(false);
@@ -95,6 +110,7 @@
         private System.Windows.Forms.Label capriLabel;
         private System.Windows.Forms.Label date1Label;
         private System.Windows.Forms.Label date2Label;
+        private System.Windows.Forms.Label nameLabel;
     }
 }
 
